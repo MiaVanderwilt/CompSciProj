9f4479bede5cd2c822fc38a87518c11438ec51fe
ros_mcvanderwilt <ros_mcvanderwilt@WROSRM31922.seattleschools.org>
2014-09-04 08:42:22 -0700

    Initial commit

---
+++ Factoring/src/Factor.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+//Assignment One
+
+
+public class Factor {
+	
+	public static void main (String[] args){
+		
+		//prompt input
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input integer between 1-100: ");
+		//TODO: account for other types of input
+		if (scanner.hasNextInt()) {
+			//int input = 
+			System.out.println("Factor");
+		} else {
+			System.out.println("Error");
+			//error message
+		}
+			
+
+		//parse input
+		//factor, quit, or error message
+		
+	}
+	
+	//Factor class: take int and divide
+
+	//Return array list of factors
+	
+}
